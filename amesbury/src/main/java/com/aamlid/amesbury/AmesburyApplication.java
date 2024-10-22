001e74fb093f85bd9ff7bad12b12be3693c61277
thesupermattman <84187947+thesupermattman@users.noreply.github.com>
2024-10-22 22:24:19 +0700

    Add files via upload

---
+++ amesbury/src/main/java/com/aamlid/amesbury/AmesburyApplication.java
@@ -0,0 +1,13 @@
+package com.aamlid.amesbury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmesburyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmesburyApplication.class, args);
+	}
+
+}
---
+++ amesbury/src/test/java/com/aamlid/amesbury/AmesburyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aamlid.amesbury;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmesburyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
